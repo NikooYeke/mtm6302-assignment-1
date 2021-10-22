038b74cff76dd2c21bd79cb72ccb3b2d019c8db6
Nikoo Yekehbash <yeke0002@algonquinlive.com>
2021-10-22 13:10:04 -0400

    starting emoji gallery

---
+++ script.js
@@ -0,0 +1,14 @@
+//Data
+const $container = document.getElementById('container')
+
+const emojis = `<div><div>&#129409</div><code>129409<code></div> <div><div>&#127790</div><code>127790<code></div> <div ><div>&#127789</div><code>127789<code></div> <div><div>&#127791</div><code>127791<code></div> <p><div>&#127792</div><code>127792<code></p> <p><div>&#127793</div><code>127793<code></p> <p><div>&#127794</div><code>127794<code></p> <p><div>&#127795</div><code>127795<code></p> <p><div>&#127796</div><code>127796<code></p> <p><div>&#127797</div><code>127797<code></p> <p><div>&#127798</div><code>127798<code></p> <p><div>&#127799</div><code>127799<code></p> <p><div>&#127800</div><code>127800<code></p> <p><div>&#127801</div><code>127801<code></p> <p><div>&#127802</div><code>127802<code></p> <p><div>&#127803</div><code>127803<code></p> <p><div>&#127804</div><code>127804<code></p> <p><div>&#127805</div><code>127805<code></p> <p><div>&#127806</div><code>127806<code></p> <p><div>&#127807</div><code>127807<code></p> <p><div>&#127808</div><code>127808<code></p> <p><div>&#127809</div><code>127809<code></p> <p><div>&#127810</div><code>127810<code></p> <p><div>&#127811</div><code>127811<code></p> <p><div>&#127812</div><code>127812<code></p> <p><div>&#127813</div><code>127813<code></p> <p><div>&#127814</div><code>127814<code></p> <p><div>&#127815</div><code>127815<code></p> <p><div>&#127816</div><code>127816<code></p> <p><div>&#127817</div><code>127817<code></p> <p><div>&#127818</div><code>127818<code></p> <p><div>&#127819</div><code>127819<code></p> <p><div>&#127820</div><code>127820<code></p> <p><div>&#127821</div><code>127821<code></p> <p><div>&#127822</div><code>127822<code></p> <p><div>&#127823</div><code>127823<code></p> <p><div>&#127824</div><code>127824<code></p> <p><div>&#127825</div><code>127825<code></p> <p><div>&#127826</div><code>127826<code></p> <p><div>&#127827</div><code>127827<code></p> <p><div>&#127828</div><code>127828<code></p> <p><div>&#127829</div><code>127829<code></p> <p><div>&#127830</div><code>127830<code></p> <p><div>&#127831</div><code>127831<code></p> <p><div>&#127832</div><code>127832<code></p> <p><div>&#127833</div><code>127833<code></p> <p><div>&#127834</div><code>127834<code></p> <p><div>&#127835</div><code>127835<code></p> <p><div>&#127836</div><code>127836<code></p> <p><div>&#127837</div><code>127837<code></p> <p><div>&#127838</div><code>127838<code></p>`
+
+
+//Code
+
+//Run
+$container.innerHTML += emojis
+
+console.log(emojis)
+
+
